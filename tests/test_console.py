6f06b3ea8d0ed1efd86d3580532438526f447187
root <godrace062@gmail.com>
2022-11-24 11:01:59 -0800

    Task

---
+++ tests/test_console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unit test for the file storage class
+"""
+import unittest
+from console import HBNBCommand
+
+
+class TestConsoleClass(unittest.TestCase):
+    """TestConsoleClass resume
+    Args:
+        unittest (): Propertys for unit testing
+    """
+    
+    def test_func(self):
+        """To test" func"""
+        pass
+
+    def test_func2(self):
+        """To test func 2"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
