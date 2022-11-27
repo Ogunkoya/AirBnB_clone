9fc454ab395a7e5aa0e61b40da643d5d78806e26
root <godrace062@gmail.com>
2022-11-27 06:36:56 -0800

    Hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -25,9 +25,9 @@ class BaseModel:
             if hasattr(self, "updated_at") and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            self.id = str(uuid.uuid4())
             models.storage.new(self)
             models.storage.save()
 
