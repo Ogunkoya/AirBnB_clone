f80d7cae9b79811cb12f55b81ab2073c386aa9bd
hilerioluwa12 <hardexilerioluwa2gmail.com>
2022-11-27 11:19:48 -0800

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -2,9 +2,15 @@
 """
 Module: __init__.py
 """
+from . import engine
+from . import base_model
+
+
+
 
 from models.engine.file_storage import FileStorage
 
 
 storage = file_storage.FileStorage()
+
 storage.reload()
